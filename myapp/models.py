c23f97f0a4fe10c8aa0b9129fcb3137f38fd7e07
Anas Saifeee <anas.saifi@iic.ac.in>
2022-08-20 17:53:09 +0530

    commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.db import models
+from django.contrib.auth.forms import User
 
 # Create your models here.
 class Donor_form(models.Model):
@@ -17,4 +18,13 @@ class Donor_form(models.Model):
     time = models.CharField(max_length = 122)
     e_img = models.FileField(upload_to='', storage=None, max_length=100)
     date = models.DateField()
+
+class extendeduser(models.Model):
+     name = models.CharField(max_length=20)
+     aadhaar_no = models.CharField(max_length=20, null = True)
+     email =  models.CharField(max_length = 12,null= True)
+     password = models.CharField(max_length = 20,null = True)
+     username = models.CharField(max_length = 20,null = True)
+     def __str__(self):
+        return self.username
     
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,12 @@
 from http.client import HTTPResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render , HttpResponse,redirect
 from myapp.models import Donor_form
+from django.contrib.auth.forms import User
 from datetime import datetime
+from django.contrib.auth import authenticate,logout,login
+from django.contrib import messages
+from .models import extendeduser
+
 
 # Create your views here.
 def index(request):
@@ -14,14 +19,55 @@ def about(request):
 def services(request):
     return HttpResponse("this is servicespage")
 
-def login(request):
-    return render(request,'login.html')
-
-def login2(request):
-    return render(request,'login2.html')
-
 def signup(request):
-    return render(request,'signup.html')
+   if request.method == "POST":
+      name = request.POST.get("name") 
+      username = request.POST.get("username")
+      email =  request.POST.get("email")
+      aadhaarno = request.POST.get("aadhaarnumber")
+      password = request.POST.get("password")
+      confirmpassword = request.POST.get("confirmpassword")
+      if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please try some other username.")
+            return redirect('signup')
+        
+      if User.objects.filter(email=email).exists():
+            messages.error(request, "Email Already Registered!!")
+            return redirect('signup')
+        
+      if len(username)>20:
+            messages.error(request, "Username must be under 20 charcters!!")
+            return redirect('signup')
+        
+      if password != confirmpassword:
+            messages.error(request, "Passwords didn't matched!!")
+            return redirect('signup')
+        
+      if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!!")
+            return redirect('signup')  
+      myuser = User.objects.create_user(username,email,password)
+      myuser.save()
+      signup = extendeduser(name = name,email = email,username = username,aadhaar_no = aadhaarno,password=password)
+      signup.save()
+     
+      messages.success(request,"your account has been successfully created")
+      return redirect('signin')
+      
+   return render(request,"signup.html")
+
+def signin(request):
+   if request.method == 'POST':
+      username = request.POST['username']
+      password = request.POST['password']
+      
+      user = authenticate(username=username,password=password)
+      if user is not None:
+         login(request,user)
+         return render(request,"dashboard.html")
+      else:
+         return redirect(signin)
+   return render(request,"signin.html")
 
 def guidelines(request):
     return render(request,'guidelines.html')
