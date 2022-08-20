59c54a30b1c0d4eef1836339b227cb6894068549
abhishekty17 <abhishekty17@gmail.com>
2022-08-20 17:16:42 +0530

    commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from myapp.models import Donor_form
+from myapp.models import extendeduser
 # Register your models here.
 admin.site.register(Donor_form)
+admin.site.register(extendeduser)
